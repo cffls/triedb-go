072f41868bf54d397733d0e66fd8de5dd384de80
Jerry <jerrycgh@gmail.com>
2025-11-17 15:45:08 -0800

    Fix: Load cbindgen.toml config in build.rs for consistent header generation

--- triedb-ffi/build.rs
+++ triedb-ffi/build.rs
@@ -1,13 +1,13 @@
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let config_path = PathBuf::from(&crate_dir).join("cbindgen.toml");
 
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .with_pragma_once(true)
-        .with_include_guard("TRIEDB_H")
+        .with_crate(&crate_dir)
+        .with_config(cbindgen::Config::from_file(&config_path).unwrap())
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("triedb.h");
